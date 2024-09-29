a96cc1d47ee44e46d1b05c2d3abcd46b28ee4318
luvlestudio <sowoojoo31@gmail.com>
2024-09-29 23:50:59 +0900

    Update custom.js

--- src/custom/custom.js
+++ src/custom/custom.js
@@ -9,7 +9,7 @@ export const OG_IMAGE_URL =
   "https://image.cine21.com/resize/cine21/still/2005/1121/M0020066_focus52804[W578-].jpg";
 
 // 꽃잎 효과
-export const 꽃잎효과ONOFF = false; // 꽃잎효과를 키고 싶다면 true, 끄고 싶다면 false로 변경
+export const 꽃잎효과ONOFF = true; // 꽃잎효과를 키고 싶다면 true, 끄고 싶다면 false로 변경
 
 /**
  * 신랑 신부
